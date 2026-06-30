1d9f4b325c727cf2bacca7cf90dd3933060fe8c1
ndom91 <yo@ndo.dev>
2026-06-30 10:28:24 +0200

    Update hassio post standardsite publish

--- src/lib/standard-site.generated.ts
+++ src/lib/standard-site.generated.ts
@@ -1,2 +1,1 @@
-export const standardSitePublicationUri =
-  'at://did:plc:nmwj6xne7mdpkry5pzvfendv/site.standard.publication/ndo-dev'
+export const standardSitePublicationUri = 'at://did:plc:nmwj6xne7mdpkry5pzvfendv/site.standard.publication/ndo-dev'
